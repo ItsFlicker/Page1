71bba938cdbed75e421c8b397a8dadb7f6af88e6
ItsFlicker <1812496278@qq.com>
2019-04-27 20:00:35 +0800

    V1.01

--- zs.js
+++ zs.js
@@ -1,8 +1,7 @@
 function zspd() {
     var x = document.forms["zs"]["zssr"].value;
-    flag = true
     if (x < 2){
-        alert("数据不合法!");
+        alert("数据不合法!(请输入大于1的数)");
         return false;
     }
 
@@ -13,13 +12,10 @@ function zspd() {
     for (var a = 2;a <= x / 2;a++){
         if (x % a === 0){
             alert(x + "是一个合数!" + "\n" + "它的因数除了1和它本身还有" + a + "!");
-            flag = false;
             return true;
         }
     }
-    if (flag === true)
-    {
-        alert(num + "是一个质数!")
-        return true
-    }
+
+    alert(num + "是一个质数!");
+    return true
 }
