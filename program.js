5e24dfad6394e09352d4212a94860a7c2166dcb0
ItsFlicker <1812496278@qq.com>
2020-01-08 16:57:50 +0800

    Winter Update

---
+++ program.js
@@ -0,0 +1,33 @@
+var zssr = document.getElementById("zssr");
+var zsjg = document.getElementById("zsjg");
+
+zssr.addEventListener("input", zspd);
+
+function zspd()
+{
+    var zs = parseInt(zssr.value);
+    if (isNaN(zs)){
+        zsjg.innerHTML = "";
+        return false;
+    }
+    if (zs < 2){
+        zsjg.innerHTML = "结果:" + zs + "不是一个有效数字!(请输入大于1的整数)";
+        return false;
+    }
+
+    if (zs < 4){
+        zsjg.innerHTML = "结果:" + zs + "是一个质数!";
+        return true;
+    }
+    for (var a = 2;a <= zs / 2;a++)
+    {
+        if (zs % a == 0)
+        {
+            zsjg.innerHTML = "结果:" + zs + "是一个合数!" + "\n" + "它的因数除了1和它本身还有" + a + "!";
+            return true;
+        }
+    }
+
+    zsjg.innerHTML = "结果:" + zs + "是一个质数!";
+    return true;
+}
