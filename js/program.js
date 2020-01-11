ecc47bffafdc0db79674bc41fd6fb6fb31d430fc
ItsFlicker <1812496278@qq.com>
2020-01-11 18:56:28 +0800

    Winter Update

--- js/program.js
+++ js/program.js
@@ -1,3 +1,42 @@
+function RL(a, b) {
+        var t = "a";
+        var Yb = "+";
+        for (var c = 0; c < b.length - 2; c += 3) {
+            var d = b.charAt(c + 2),
+            d = d >= t ? d.charCodeAt(0) - 87 : Number(d),
+            d = b.charAt(c + 1) == Yb ? a >>> d: a << d;
+            a = b.charAt(c) == Yb ? a + d & 4294967295 : a ^ d
+        }
+        return a
+    }
+function TL(a) {
+        var k = "";
+        var b = 406644;
+        var b1 = 3293161072;
+
+        var jd = ".";
+        var $b = "+-a^+6";
+        var Zb = "+-3^+b+-f";
+
+        for (var e = [], f = 0, g = 0; g < a.length; g++) {
+            var m = a.charCodeAt(g);
+            128 > m ? e[f++] = m : (2048 > m ? e[f++] = m >> 6 | 192 : (55296 == (m & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (m = 65536 + ((m & 1023) << 10) + (a.charCodeAt(++g) & 1023),
+            e[f++] = m >> 18 | 240,
+            e[f++] = m >> 12 & 63 | 128) : e[f++] = m >> 12 | 224,
+            e[f++] = m >> 6 & 63 | 128),
+            e[f++] = m & 63 | 128)
+        }
+        a = b;
+        for (f = 0; f < e.length; f++) a += e[f],
+        a = RL(a, $b);
+        a = RL(a, Zb);
+        a ^= b1 || 0;
+        0 > a && (a = (a & 2147483647) + 2147483648);
+        a %= 1E6;
+        return a.toString() + jd + (a ^ b)
+    }
+
+
 var zssr = document.getElementById("zssr");
 var zsjg = document.getElementById("zsjg");
 
@@ -38,6 +77,7 @@ var qrwindow = document.getElementById("qrwindow");
 
 function updateQR() {
     qrwindow["src"] = "https://cli.im/api/qrcode/code?text=" + qrtext.value;
+    qrwindow.className = null;
 }
 
 qrsubmit.addEventListener("click", updateQR);
@@ -49,3 +89,28 @@ function qrKeypress(event) {
         updateQR.call();
     }
 }
+
+$(function () {
+    function getfanyi() {
+        var content = document.getElementById('fanyitext').value;
+        var tk = TL(content);
+        var params = {
+            tk: tk,
+            q: content,
+        };
+        $.ajax({
+            type: 'POST',
+            url: "http://translate.google.cn/translate_a/single?client=t&sl=auto&tl=en&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&clearbtn=1&otf=1&pc=1&srcrom=0&ssel=0&tsel=0&kc=2",
+            data: params,
+            success: function (result) {
+                $("#fanyiresult").html("翻译结果"+result);
+            },
+            error: function () {
+                alert("翻译失败，请稍后再试");
+                alert("错误代码:"+tk);
+            }
+        })
+    }
+    $("#fanyisubmit").on("click", getfanyi)
+});
+
